95a95c7e2a90a9c4f34cc1b26b6683f1e7373002
Werefox <jack@wrightus.com>
2022-08-20 13:22:15 -0400

    Created some palindrome functions for solution 5

--- solution5.py
+++ solution5.py
@@ -1,1 +1,27 @@
 # Write code for algorithm 5 below
+def is_palindrome_short(string):
+	return string == string[::-1]
+
+
+def is_palindrome_interation(string):
+	for i in range(0, len(string) // 2):
+		if string[i] != string[0 - i - 1]:
+			return False
+	return True
+
+def is_palindrome_recursion(string):
+	print(string)
+	# base case
+	if len(string) <= 2:
+		return string[0] == string[-1]
+	# recursive case
+	else:
+		val = is_palindrome_recursion(string[1:-1])
+		print(val)
+		return val
+
+strings = ["racecar", "madam", "panama", "tacocat", "shrek"]
+for i in strings:
+	print(f"short: {i} is {('not ', '')[is_palindrome_short(i)]}a palindrome")
+	print(f"iteration: {i} is {('not ', '')[is_palindrome_interation(i)]}a palindrome")
+	print(f"recursion: {i} is {('not ', '')[is_palindrome_recursion(i)]}a palindrome\n")
